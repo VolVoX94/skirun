3312907b65faeb0e3f43aa4691aff07e2cb6278c
Stefan Eggenschwiler <stefan.eggenschwiler@hevs.ch>
2019-02-25 13:32:15 +0100

    Initial Commit

---
+++ skirun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  skirun
+//
+//  Created by Admin on 25.02.19.
+//  Copyright © 2019 GROUP2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
