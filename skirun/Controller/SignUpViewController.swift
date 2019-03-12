b3d982cbd2ae677b7b39d3017058985d5cef6eac
Danilo Marty <danilo.marty@students.hevs.ch>
2019-03-12 15:21:59 +0100

    Registercontroller updated

--- skirun/Controller/SignUpViewController.swift
+++ skirun/Controller/SignUpViewController.swift
@@ -25,16 +25,38 @@ class SignUpViewController: UIViewController {
     
     
     @IBAction func nextButtonFunc(_ sender: Any) {
+        
+        //Check if email is correct
         if(isValidEmail(testStr: emailField.text!)){
-            if(passwordField.text?.count ?? 0 >= 6){
-                let nextController:DetailsSignupViewController = DetailsSignupViewController();
+            
+            //check if pw is big enough and equal to repeated pw
+            if((passwordField.text?.count ?? 0 >= 6)){
                 
-                //Send data to the next controller
-                nextController.email = emailField.text;
-                nextController.password = passwordField.text;
-                
-                //Open next view
-                self.present(nextController, animated: true, completion: nil);
+                //Check if pw equal to repeatedPw
+                if(repeatPwField.text == passwordField.text){
+                    
+                    //INPUT IS OK
+                    let nextController:DetailsSignupViewController = DetailsSignupViewController();
+                    
+                    //Send data to the next controller
+                    nextController.email = emailField.text;
+                    nextController.password = passwordField.text;
+                    
+                    //Open next view
+                    self.present(nextController, animated: true, completion: nil);
+                    
+                    
+                }
+                else{
+                    let alertPassword = UIAlertController(
+                        title: "Password not accepted",
+                        message: "Passwordinput is not equal",
+                        preferredStyle: .actionSheet);
+                    
+                    alertPassword.addAction(UIAlertAction(title: "Ok", style: .cancel, handler:nil))
+                    
+                    self.present(alertPassword, animated: true);
+                }
                 
             }
                 //wrong password
@@ -44,17 +66,21 @@ class SignUpViewController: UIViewController {
                     message: "Please enter a password with at least 6 charachters",
                     preferredStyle: .actionSheet);
                 
+                alertPassword.addAction(UIAlertAction(title: "Ok", style: .cancel, handler:nil))
+                
                 self.present(alertPassword, animated: true);
             }
         }
             //wrong email
         else{
-            let alertPassword = UIAlertController(
+            let alertEmail = UIAlertController(
                 title: "Email not accepted",
                 message: "Email should be like xyz@xy.xy",
                 preferredStyle: .actionSheet);
             
-            self.present(alertPassword, animated: true);
+            alertEmail.addAction(UIAlertAction(title: "Ok", style: .cancel, handler:nil))
+            
+            self.present(alertEmail, animated: true);
         }
     }
     
