e11701bed37c8960a8b3b10b35ae4b89d217dc66
RebeloHugo <hugo.rebelo@students.hevs.ch>
2019-03-13 10:06:30 +0100

    Admin first step (main page, management page)

---
+++ skirun/Controller/AdminViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AdminViewController.swift
+//  skirun
+//
+//  Created by iOS Dev on 13.03.19.
+//  Copyright © 2019 hevs. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
