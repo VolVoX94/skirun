8959ffa1054184e459d2a5a06502cf81e9263388
Danilo Marty <danilo.marty@students.hevs.ch>
2019-03-20 09:37:46 +0100

    Model - Competition

--- skirun/Controller/FirstViewController.swift
+++ skirun/Controller/FirstViewController.swift
@@ -3,7 +3,10 @@
 import UIKit
 import Firebase
 
-class FirstViewController: UIViewController {
+class FirstViewController: UITableViewController {
+    
+    var myCompetition:Competition?;
+    let competitions = [myCompetition];
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,26 @@ class FirstViewController: UIViewController {
     //give information to next view
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         print("test")
+    }
+    
+    func loadData(uid:String){
+        //Get data for the list
+        let competitionSession:FirebaseSession = FirebaseSession.competition;
+        
+        var ref: DatabaseReference!
+        ref = Database.database().reference();
+        
+        ref.child(competitionSession.rawValue).childByAutoId()
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 
 
