a0635505fe3d0708c750dfe7e4e9428d13bd5d37
Danilo Marty <danilo.marty@students.hevs.ch>
2019-03-25 13:06:02 +0100

    list availability update

--- skirun/Controller/FirstViewController.swift
+++ skirun/Controller/FirstViewController.swift
@@ -1,84 +1,38 @@
 
 
 import UIKit
-import Firebase
-import FirebaseDatabase
 
 class FirstViewController: UIViewController, UITableViewDataSource{
     
-    var myCompetition:Competition?
-    var competitions:[Competition] = []
+    private var data:[String] = []
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
         tableView.dataSource = self
         
-        //Load data from firebase
-        loadData()
-        
-        
+        FirebaseManager.getCompetitons(completion: { (data) in
+            self.data = Array(data)
+        })
     }
     
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func showDetails(_ sender: Any) {
-        //Open next view, prepare method will be launched
-        performSegue(withIdentifier: "MyAvailabiltySegue", sender: self)
-        
-    }
-    
-    //give information to next view
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("test")
-    }
-    
-    func loadData(){
-        //Get data for the list
-        let competitionSession:FirebaseSession = FirebaseSession.competition;
-        
-        var ref: DatabaseReference!
-        ref =  Database.database().reference()
-        
-        ref.child(competitionSession.rawValue).queryOrderedByKey().observe(.childAdded) { (snapshot) in
-            let snapValue = snapshot.value as! NSDictionary
-            
-            //Create temp object
-            self.myCompetition = Competition(
-                name: snapshot.key,
-                startDateTime: snapValue["stardDateTime"] as? String ?? "",
-                endDateTime: snapValue["endDateTime"] as? String ?? "",
-                guestClub: snapValue["guestClub"] as? String ?? "",
-                refAPI: snapValue["refAPI"] as? String ?? "",
-                discipline: snapValue["discipline"] as? String ?? "");
-            
-            //Add the item to the list
-            self.competitions.insert(self.myCompetition!, at: 0)
-        }
-    }
-    
-    func numberOfSections(in tableView:UITableView) -> Int{
-        return 1
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("NUMBEROFROWSINSECTION---", competitions.count)
-        return competitions.count;
+        return data.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellCompetition")! //1.
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")!
+        let text = data[indexPath.row] //2.
         
-        cell.textLabel?.text = competitions[indexPath.row].name
+        cell.textLabel?.text = text //3.
         
-        print("CELLFORROWAT---", self.myCompetition?.name)
-        return cell
+        return cell //4.
     }
 }
 
