55f6fb034a4a092eebb56251d2a1cfd91bfdd820
Danilo Marty <danilo.marty@students.hevs.ch>
2019-03-26 11:18:52 +0100

    Availability update Frontend

--- skirun/Controller/DetailAvailability.swift
+++ skirun/Controller/DetailAvailability.swift
@@ -8,26 +8,64 @@
 
 import UIKit
 
-class DetailAvailability: UIViewController, UITableViewDataSource, UITableViewDelegate{
+class DetailAvailability: UIViewController, UITableViewDataSource, UITableViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource{
         
     private var data:[Competition] = []
     var name:String?
     var date:String?
     var myCompetition:Competition?
+    var pickerData: [String] = [String]()
+    
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var myCompetitionName: UILabel!
     
     @IBOutlet weak var myStartDateLabel: UILabel!
         
-        override func viewDidLoad() {
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    override func viewDidLoad() {
             super.viewDidLoad()
             tableView.dataSource = self
             
             loadCompetition(selectedCompetition: self.name!)
             self.myCompetitionName.text = self.name
             self.myStartDateLabel.text = self.name
+        
+            self.myPicker.delegate = self
+            self.myPicker.dataSource = self
+            pickerData = ["test", "banane", "affji"]
         }
+    
+    //Picker methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 30
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let view = UIView(frame: CGRect(x:0, y:0, width:100, height: 30))
+        
+        let topLabel = UILabel(frame: CGRect(x:0, y:0, width: 100, height: 25))
+        topLabel.text = pickerData[row]
+        topLabel.textColor = UIColor.white
+        topLabel.textAlignment = .center
+        topLabel.font = UIFont.systemFont(ofSize: 25)
+        view.addSubview(topLabel)
+        
+        return view
+    }
     
     func loadCompetition(selectedCompetition: String){
             FirebaseManager.getCompetiton(name: selectedCompetition , completion: { (data) in
@@ -46,19 +84,12 @@ class DetailAvailability: UIViewController, UITableViewDataSource, UITableViewDe
         
         alertBox.addAction(UIAlertAction(title: "Ok", style: .cancel, handler:nil))
         
-        
-        //Check if email is correct
-        //Will call the regex pattern method!
-        //if((isValidName(testStr: fnameField.text!) == false)){
-            
             //Define that something is wrong
             alertBox.message = "You have to select a mission";
             
             //Display the alertBox
             self.present(alertBox, animated: true);
         }
-    
-    
     
     @IBAction func nextButton(_ sender: Any) {
             performSegue(withIdentifier: "MyNextSegue", sender: self)
