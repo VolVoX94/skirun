c88dfa3ee4407a20c8b0f4085de4b104b1daf4a1
Aurélie <aurelie.glassey@students.hevs.ch>
2019-04-02 09:38:10 +0200

    Add two picker in mainstoryboard

--- skirun/Controller/SecondViewController.swift
+++ skirun/Controller/SecondViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by AISLAB on 27.02.19.
 //  Copyright © 2019 hevs. All rights reserved.
-// Olivier test
+
 
 
 
@@ -12,28 +12,33 @@ import UIKit
 
 class SecondViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource {
     
+    var selectedCompetition: String?
+    var selectedDiscipline: String?
 
-    
-
+    // TAG -> 0
     @IBOutlet weak var competitionPicker: UIPickerView!
     var listCompetitions:[String] = [String]()
     
+    // TAG -> 1
+    @IBOutlet weak var disciplinePicker: UIPickerView!
+    var listDisciplines:[String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.competitionPicker.delegate = self
-        self.competitionPicker.dataSource = self
+        
+        
         loadListCompetitions();
+        //loadListDisciplines();
         
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        //if (pickerView.tag == 0){
-            return listCompetitions[row]
-       // }else{
-             //-----------
-        //    return listCompetitions[row]
-        //}
+        if (pickerView.tag == 0){
+            return "\(listCompetitions[row])"
+        }else{
+            return "\(listDisciplines[row])"
+        }
         
     }
     
@@ -42,35 +47,77 @@ class SecondViewController: UIViewController , UIPickerViewDelegate, UIPickerVie
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        // if (competitionPicker.tag == 0){
-        return listCompetitions.count
-        // }else{
-        //-----------
-        //    return listCompetitions.count
-        // }
+        if (pickerView.tag == 0){
+            return listCompetitions.count
+        }else{
+            return listDisciplines.count
+        }
+    }
+    
+    //Check wich element has been choosen
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        if(pickerView.tag == 0){
+            self.selectedCompetition = listCompetitions[row]
+            
+        }else{
+            self.selectedDiscipline = listDisciplines[row]
+            
+        }
+        
     }
     
     func loadListCompetitions(){
         FirebaseManager.getCompetitons(completion: { (data) in
             self.listCompetitions = Array(data)
+            self.competitionPicker.delegate = self
+            self.competitionPicker.dataSource = self
             print(data)
         })
     }
     
+    func loadListDisciplines(){
+        /*FirebaseManager.getDisciplinesOfCompetition(name: self.selectedCompetition!) { (pickerData) in
+            self.pickerData = Array(pickerData)
+            self.disciplinePicker.delegate = self
+            self.disciplinePicker.dataSource = self
+            self.pickerData.insert("Please select", at: 0)
+        }
+ */
+    }
+    
+ 
     //Picker formatting
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let view = UIView(frame: CGRect(x:0, y:0, width:400, height: 30))
         
-        let topLabel = UILabel(frame: CGRect(x:0, y:0, width: 400, height: 14))
-        topLabel.text = listCompetitions[row]
-        topLabel.textColor = UIColor.white
-        topLabel.textAlignment = .center
-        topLabel.font = UIFont.systemFont(ofSize: 14)
-        view.addSubview(topLabel)
+        if (pickerView.tag == 0){
+            let view = UIView(frame: CGRect(x:0, y:0, width:400, height: 30))
+            
+            let topLabel = UILabel(frame: CGRect(x:0, y:0, width: 400, height: 14))
+            topLabel.text = listCompetitions[row]
+            topLabel.textColor = UIColor.white
+            topLabel.textAlignment = .center
+            topLabel.font = UIFont.systemFont(ofSize: 14)
+            view.addSubview(topLabel)
+            
+            return view
+            
+        }else{
+            let view = UIView(frame: CGRect(x:0, y:0, width:400, height: 30))
+            
+            let topLabel = UILabel(frame: CGRect(x:0, y:0, width: 400, height: 14))
+            topLabel.text = listDisciplines[row]
+            topLabel.textColor = UIColor.white
+            topLabel.textAlignment = .center
+            topLabel.font = UIFont.systemFont(ofSize: 14)
+            view.addSubview(topLabel)
+            
+            return view
         
-        return view
+        
+        }
     }
 
-
+ 
 }
 
