e9b74b299c36fa0431ab17630d4551619c35338c
Aurélie <aurelie.glassey@students.hevs.ch>
2019-04-16 15:49:54 +0200

    Adding a file for new tests

---
+++ skirunTests/viewControllerTest.swift
@@ -0,0 +1,13 @@
+//
+//  viewControllerTest.swift
+//  skirunTests
+//
+//  Created by Rey Elodie on 16.04.19.
+//  Copyright © 2019 hevs. All rights reserved.
+//
+
+import UIKit
+
+class viewControllerTest: XCTestCase {
+
+}
